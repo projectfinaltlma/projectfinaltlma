bb1f6b1c2c1302fc2eee0fa71b895354a12c2c49
projectfinaltlma <projectfinaltlma@gmail.com>
2022-11-30 00:35:34 -0600

    cambio

--- cmp/mi-nav.js
+++ cmp/mi-nav.js
@@ -47,7 +47,7 @@ class MiNav extends HTMLElement {
         html += /* html */
           `<li>
             <a href=
-"alumnos.html">Alumnos</a>
+"alumnos.html">Tlapaleria</a>
           </li>`;
       }
       this.ul.innerHTML += html;
